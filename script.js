3c394b4140bf5eb8d15ba75fb88456ca594e5cef
rakibul58 <rhrahi14@gmail.com>
2022-09-14 21:12:05 +0600

    time seconds without decimal

--- script.js
+++ script.js
@@ -68,6 +68,7 @@ const gameOver = () => {
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
   const timeTaken = (finishTime - startTime) / 1000;
+  
 
   // show result modal
   resultModal.innerHTML = "";
@@ -133,7 +134,7 @@ displayHistory();
 setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = (currentTime - startTime) / 1000;
+  
 
-
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${startTime ? Math.round(timeSpent) : 0} seconds`;
 }, 1000);
